cf1881d248b0271d042e4ad98822f16aaf608399
manav132 <manav132@gmail.com>
2025-03-19 17:26:01 -0400

    update job script

--- experiment/job_script.sh
+++ experiment/job_script.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
 #SBATCH --gpus=1
-#SBATCH --mem-per-cpu=5120
+#SBATCH --mem=5120
 #SBATCH --time=01:00:00   # walltime
 #SBATCH --error=err.txt
 
@@ -11,6 +11,6 @@ module load gcc/8.2.0 python_gpu
 
 echo "Starting run at: `date`"
 
-python main.py
+python train_model.py
 
 echo "Program finished with exit code $? at: `date`"
